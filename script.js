b57e195d261e9f4556e2e5e0ac70dd08d7e5c8d4
Cory Ellerbe <Coco@Corys-MacBook-Pro.local>
2020-02-24 14:42:53 -0600

    finished form validator

--- script.js
+++ script.js
@@ -20,14 +20,61 @@ function showSuccess(input) {
   formControl.className = 'form-control success';
 }
 
+// Check the Email is Valid 
+
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
+}
+
+// Check Required Fields 
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) {
+    if(input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Check Input Length
+function checkLength(input, min, max) {
+  if(input.value.length < min) {
+    showError(
+      input, `${getFieldName(input)} must be at least ${min} characters`);
+  } else if(input.value.length > max) {
+    showError(
+      input, `${getFieldName(input)} must be less than ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Check Password Match
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords must match");
+  }
+}
+
+// Get Field Name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // Event Listeners 
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  if(username.value === '') {
-    showError(username, 'Username Required');
-  } else {
-    showSuccess(username);
-  }
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordMatch(password, password2);
 });
